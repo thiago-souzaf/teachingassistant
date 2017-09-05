7d3f30ed3a192d531d641b171781189ad1b1df13
pauloborba <phmb@cin.ufpe.br>
2017-09-05 18:34:00 -0300

    cadastro de metas, e clonagem de objetos para simular funcionamento do servidor

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -3,4 +3,34 @@ export class Aluno {
   cpf: string = "";
   email: string = "";
   login_gh: string = "";
+  metas: Map<string,string> | undefined;
+
+  constructor() {
+    this.clean();
+  }
+
+  clean(): void {
+    this.nome = "";
+    this.cpf = "";
+    this.email = "";
+    this.metas = new Map<string,string>();
+  }
+
+  clone(): Aluno {
+    var aluno: Aluno = new Aluno();
+    aluno.nome = this.nome;
+    aluno.cpf = this.cpf;
+    aluno.email = this.email;
+    aluno.metas = this.cloneMetas();
+    return aluno;
+  }
+
+  cloneMetas(): Map<string,string> {
+    var metas: Map<string,string> = new Map<string,string>();
+    for (let key in this.metas) {
+      var metasValues = metas.get(key)
+      metasValues = this.metas.get(key);
+    }
+    return metas;
+  }
 }
