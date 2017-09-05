0c2824d3a22cf71eb6e6202639778a9c2f790441
pauloborba <phmb@cin.ufpe.br>
2017-09-05 19:24:27 -0300

    modularizando o componente principal

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Aluno } from './aluno';
 @Injectable()
-
 export class AlunoService {
   alunos: Aluno[] = [];
-  criar(aluno: Aluno): Aluno | null {
+  criar(aluno: Aluno): Aluno | null{
     aluno = aluno.clone();
     var result = null;
-    if (this.cpfNaoCadastrado(aluno.cpf as string)) {
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
     return result;
   }
   cpfNaoCadastrado(cpf: string): boolean {
-    return !this.alunos.find(a => a.cpf == cpf);
- }
+     return !this.alunos.find(a => a.cpf == cpf);
+  }
 
-  atualizar(aluno:Aluno): void {
+  atualizar(aluno: Aluno): void {
     aluno = aluno.clone();
     for (let a of this.alunos) {
         if (a.cpf == aluno.cpf) {
-          a.metas = aluno.metas;
+           a.metas = aluno.metas;
         }
     }
+  }
+
+  getAlunos(): Aluno[] {
+    var result: Aluno[] = [];
+    for (let a of this.alunos) {
+      result.push(a.clone());
+    }
+    return result;
   }
 }
--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -24,7 +24,4 @@ export class AppComponent {
   onmove(): void {
     this.cpfduplicado = false;
   }
-  atualizarAluno(aluno: Aluno): void {
-    this.alunoService.atualizar(aluno);
- }
 }
