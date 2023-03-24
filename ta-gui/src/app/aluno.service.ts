38137eba6998e04a982fe5ec293aaf3629c97a25
Thiago Farias <tsf@cin.ufpe.br>
2023-03-24 16:56:18 -0300

    corrigido bug em aluno.service.ts

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -2,7 +2,8 @@ import { Aluno } from './aluno';
 
 export class AlunoService {
   alunos: Aluno[] = [];
-  gravar(aluno: Aluno): Aluno {
+
+  gravar(aluno: Aluno): void {
      this.alunos.push(aluno);
   }
 }
